18cebeace941d156ab3375b45709dbd0b422bc6e
emmashek <shek.em@northeastern.edu>
2023-12-03 17:02:50 -0500

    red choropleth

--- 12.02_dash.py
+++ 12.02_dash.py
@@ -13,25 +13,6 @@ df = pd.read_csv('Asthma RPM State Coverage.csv')
 combined_df = asthma_pop_df.merge(coverage_df, left_on='States', right_on='State')
 
 
-# Generate asthma population choropleth graph
-def generate_fig(demographic):
-    fig = px.choropleth(
-        asthma_pop_df,
-        locations='State Code',  # State Code as locations
-        color=demographic,  # Color scale on population
-        locationmode='USA-states',  # Set location mode to US
-        scope='usa',  # Set scope to US
-        hover_name='States',  # Hover show state names
-        height=600,
-        color_continuous_scale='Blues',  # Use selected color scale
-        # title='Asthma Prevalence in the United States',
-    )
-
-    fig.update_coloraxes(colorbar=dict(title='Population Count'))
-
-    return fig
-
-
 # Initialize the Dash app
 app = dash.Dash(__name__,external_stylesheets=[
     {
@@ -128,46 +109,6 @@ tab2_layout = html.Div([
     ]),
 ])
 
-# # Define the layout of the app with dropdown and graph
-# app.layout = html.Div([
-#     html.H1("Asthma Dashboard", style={'fontFamily': 'Montserrat', 'fontSize': '36px', 'marginLeft': '20px'}),
-#     html.H2("Asthma Prevalence and RPM Insurance Coverage",  style={'marginLeft': '40px', 'fontFamily': 'Montserrat', 'fontSize': '20px'}),
-#     dcc.Checklist(
-#         id='checkboxes',
-#         options=[
-#             {'label': 'Medicare', 'value': '99454 Coverage: Medicare'},
-#             {'label': 'Medicaid', 'value': '99454 Coverage: Medicaid'},
-#             {'label': 'Top Private Insurance', 'value': '99454 Coverage: Top Private Insurance'},
-#             {'label': 'Second Private Insurance', 'value': '99454 Coverage: Second Private Insurance'},
-#         ],
-#         value=['99454 Coverage: Medicare',
-#                '99454 Coverage: Medicaid',
-#                '99454 Coverage: Top Private Insurance',
-#                '99454 Coverage: Second Private Insurance'
-#         ],
-#         style={'width': '150px', 'fontFamily': 'Montserrat', 'marginLeft': '40px'},
-#         inline=True,
-#     ),
-#     dcc.Graph(
-#         id='choropleth-map', style={'width': '100vw', 'height': '100vh'}
-#     ),
-#     html.H2("Asthma Population by State", style={'marginLeft': '40px', 'fontFamily': 'Montserrat', 'fontSize': '20px'}),
-#     html.Div([
-#         dcc.Dropdown(
-#             id='top-n-dropdown',
-#             options=[
-#                 {'label': f'Top {i} States', 'value': i} for i in range(5, df.shape[0] + 1, 5)
-#             ],
-#             value=5,
-#             multi=False,
-#             style={'width': '50%', 'marginLeft': '40px', 'fontFamily': 'Montserrat'}
-#         ),
-#     ], style={'marginTop': '40px'}),
-#     html.Div([
-#         dcc.Graph(id='stacked-bar-chart', style={'marginTop': '100px', 'width': '100vw', 'marginLeft': '20px'})
-#     ], style={'marginLeft': '20px'})
-# ])
-
 
 # UPDATED !!!
 @app.callback(
@@ -187,7 +128,7 @@ def update_choropleth(selected_checkboxes):
         scope="usa",
         # title='Remote Patient Monitoring Coverage, CPT: 99454',
         labels={'Sum': 'Selected Columns Sum'},
-        color_continuous_scale='Blues',
+        color_continuous_scale='Reds',
         )
 
     return fig
